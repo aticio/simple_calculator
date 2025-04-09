3a10ef4dcbec4fe0fa890debc140f4a8a7a5873f
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2025-04-09 18:57:28 +0000

    step-5-multiply-two-numbers-smart

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from functools import reduce
+
 class SimpleCalculator:
     def add(self, *args):
         return sum(args)
@@ -6,5 +8,7 @@ class SimpleCalculator:
     def subtract(self, a, b):
         return a - b
 
-    def multiply(self, a, b):
-        return a * b
+    def multiply(self, *args):
+        def multiply2(a, b):
+            return a * b
+        return reduce(multiply2, args)
