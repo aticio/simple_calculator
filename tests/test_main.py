ef3c175ffbe1927dd41d39d5f09eb1447a79cab9
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2025-04-10 20:28:13 +0000

    step-9-1-average-of-an-iterable

--- tests/test_main.py
+++ tests/test_main.py
@@ -59,3 +59,9 @@ def test_mul_by_zero_raises_exception():
 
     with pytest.raises(ValueError):
         calculator.multiply(3, 0)
+
+
+def test_avg_correct_average():
+    calculator = SimpleCalculator()
+    result = calculator.avg([2, 5, 12, 98])
+    assert result == 29.25
