fec853e1e84e2fb13955addbc1f312d407456725
Özgür Atıcı <35876648+aticio@users.noreply.github.com>
2025-04-10 20:35:23 +0000

    step-9-2-upper-threshold

--- tests/test_main.py
+++ tests/test_main.py
@@ -65,3 +65,9 @@ def test_avg_correct_average():
     calculator = SimpleCalculator()
     result = calculator.avg([2, 5, 12, 98])
     assert result == 29.25
+
+
+def test_avg_removes_upper_outliers():
+    calculator = SimpleCalculator()
+    result = calculator.avg([2, 5, 12, 98], ut=90)
+    assert result == pytest.approx(6.333333)
